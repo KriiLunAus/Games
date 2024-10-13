23caeccc86571a776791a736ac385ba09f5edfea
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-13 18:05:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './css/App.css';
+import HeaderComponent from './components/HeaderComponent/HeaderComponent';
+import HeroComponent from './components/HeroComponent/HeroComponent';
+// import GameWindowComponent from './components/HeaderComponent/GameWindowComponent/GameWindowComponent';
+
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+      <HeroComponent />
+    </>
+  );
+}
+
+export default App;
