7d05d1967961caa7fa2c88d19d87a577860659d4
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-21 19:51:40 +0300

    fix flipthecoin, add images

--- src/pages/flipTheCoinPage/flipTheCoinPage.jsx
+++ src/pages/flipTheCoinPage/flipTheCoinPage.jsx
@@ -1,6 +1,8 @@
 import css from './flipTheCoinPage.module.css';
 import { useState } from 'react';
 import Total from '../../components/Total/Total.jsx';
+import heads from '../../images/svg/coinHeads.svg';
+import tails from '../../images/svg/coinTails.svg';
 
 export default function FlipTheCoinPage() {
   const [result, setResult] = useState({
@@ -9,26 +11,48 @@ export default function FlipTheCoinPage() {
     side: null,
   });
 
-  function playGame() {
-    const randomMove = Math.floor(Math.random() * 2) === 0;
+  function playGame(playerMove) {
+    const randomMove = Math.floor(Math.random() * 2) === 0 ? 'heads' : 'tails';
+    const isWin = playerMove === randomMove;
+
     setResult(prevResult => ({
-      wins: prevResult.wins + (randomMove ? 1 : 0),
-      loses: prevResult.loses + (!randomMove ? 1 : 0),
-      side: randomMove ? 'Heads' : 'Tails',
+      wins: prevResult.wins + (isWin ? 1 : 0),
+      loses: prevResult.loses + (!isWin ? 1 : 0),
+      side: randomMove,
     }));
   }
-
   return (
     <section className={css.container}>
       <h2>flip the coin</h2>
       <Total result={result} />
 
       <div id="js-result" className={css.result}>
-        {result.side}
+        {result.side === 'heads' ? (
+          <img width={200} height={200} src={heads} alt="" />
+        ) : result.side === 'tails' ? (
+          <img width={200} height={200} src={tails} alt="" />
+        ) : (
+          ''
+        )}
       </div>
+
       <div className={css.buttonWrapper}>
-        <button onClick={playGame}>Heads</button>
-        <button onClick={playGame}>Tales</button>
+        <button
+          onClick={() => {
+            playGame('heads');
+          }}
+          className="js-heads-btn"
+        >
+          Heads
+        </button>
+        <button
+          onClick={() => {
+            playGame('tails');
+          }}
+          className="js-tails-btn"
+        >
+          Tales
+        </button>
       </div>
     </section>
   );
