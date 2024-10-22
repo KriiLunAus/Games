592ac8845c4f64b1e02a55373d925eecb979eee5
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-22 18:29:19 +0300

    typos8

--- src/pages/flipTheCoinPage/FlipTheCoinPage.jsx
+++ src/pages/flipTheCoinPage/FlipTheCoinPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import css from './flipTheCoinPage.module.css';
+import css from './FlipTheCoinPage.module.css';
 import Total from '../../components/Total/Total.jsx';
 import heads from '../../images/svg/coinHeads.svg';
 import tails from '../../images/svg/coinTails.svg';
