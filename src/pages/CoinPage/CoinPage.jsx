609ad8098e608720f03ae414b7770fa5efefb50a
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-23 23:44:03 +0300

    fix coin page

--- src/pages/CoinPage/CoinPage.jsx
+++ src/pages/CoinPage/CoinPage.jsx
@@ -6,31 +6,39 @@ import tails from '../../images/svg/coinTails.svg';
 
 export default function CoinPage() {
   const [result, setResult] = useState({
-    wins: 0,
-    loses: 0,
-    side: null,
+    score: {
+      wins: 0,
+      loses: 0,
+      side: null,
+    },
   });
 
   function playGame(playerMove) {
     const randomMove = Math.floor(Math.random() * 2) === 0 ? 'heads' : 'tails';
     const isWin = playerMove === randomMove;
 
-    setResult(prevResult => ({
-      wins: prevResult.wins + (isWin ? 1 : 0),
-      loses: prevResult.loses + (!isWin ? 1 : 0),
-      side: randomMove,
-    }));
+    setResult(prevResult => {
+      const newScore = { ...prevResult.score };
+
+      newScore.wins += isWin ? 1 : 0;
+      newScore.loses += !isWin ? 1 : 0;
+      newScore.side = randomMove;
+
+      return {
+        score: newScore,
+      };
+    });
   }
 
   return (
     <section className={css.container}>
       <h2>flip the coin</h2>
-      <Total result={result} quantity={result.quantity} />
+      <Total result={result} quantity={2} />
 
       <div id="js-result" className={css.result}>
-        {result.side === 'heads' ? (
+        {result.score.side === 'heads' ? (
           <img width={200} height={200} src={heads} alt="" />
-        ) : result.side === 'tails' ? (
+        ) : result.score.side === 'tails' ? (
           <img width={200} height={200} src={tails} alt="" />
         ) : (
           ''
