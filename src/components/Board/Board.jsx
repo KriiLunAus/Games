3d8442687107c5a2118a58440693e72ddbfbdcf4
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-27 18:09:20 +0200

    sips

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -18,3 +18,34 @@ function Cell({ onClick, id }) {
     </div>
   );
 }
+
+// 1(4 l), 2(3 l), 3(2 l), 4(1 l)
+
+function Ships() {
+  const ships = [
+    {
+      id: 1,
+      length: 1,
+      position: { startId: null, orientation: null },
+      cells: [],
+    },
+    {
+      id: 2,
+      length: 2,
+      position: { startId: null, orientation: null },
+      cells: [],
+    },
+    {
+      id: 3,
+      length: 3,
+      position: { startId: null, orientation: null },
+      cells: [],
+    },
+    {
+      id: 4,
+      length: 4,
+      position: { startId: null, orientation: null },
+      cells: [],
+    },
+  ];
+}
